0265e6138362b09323b1903270b3c4808b4261c8
ArmandoCH85 <armando.correah@gmail.com>
2026-04-11 05:00:02 -0500

    update

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/api/verify-domain', function (\Illuminate\Http\Request $request) {
+    $domain = $request->query('domain');
+
+    if ($domain === 'amsolutions.lat') {
+        return response('OK', 200);
+    }
+
+    $exists = \Stancl\Tenancy\Database\Models\Domain::where('domain', $domain)->exists();
+
+    return $exists
+        ? response('OK', 200)
+        : response('Not found', 404);
+});
