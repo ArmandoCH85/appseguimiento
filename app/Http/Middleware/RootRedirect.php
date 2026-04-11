ce7e5278c06474c03c8f20300dc7d6c30c593faf
ArmandoCH85 <armando.correah@gmail.com>
2026-04-11 14:18:13 -0500

    feat: add RootRedirect middleware for central/tenant root paths

---
+++ app/Http/Middleware/RootRedirect.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class RootRedirect
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        if ($request->path() !== '/') {
+            return $next($request);
+        }
+
+        $host = $request->getHost();
+        $centralDomains = config('tenancy.central_domains', []);
+
+        if (in_array($host, $centralDomains, true)) {
+            return redirect('/central', 302);
+        }
+
+        return redirect('/app', 302);
+    }
+}
---
+++ tests/Feature/RootRedirectMiddlewareTest.php
@@ -0,0 +1,45 @@
+<?php
+
+use App\Http\Middleware\RootRedirect;
+use Illuminate\Http\Request;
+
+it('redirects root to central on central domain', function () {
+    config(['tenancy.central_domains' => ['amsolutions.lat']]);
+
+    $middleware = new RootRedirect();
+    $request = Request::create('http://amsolutions.lat/', 'GET');
+
+    $response = $middleware->handle($request, function ($req) {
+        return response('next');
+    });
+
+    expect($response->getStatusCode())->toBe(302)
+        ->and($response->getTargetUrl())->toContain('/central');
+});
+
+it('redirects root to app on tenant subdomain', function () {
+    config(['tenancy.central_domains' => ['amsolutions.lat']]);
+
+    $middleware = new RootRedirect();
+    $request = Request::create('http://demo.amsolutions.lat/', 'GET');
+
+    $response = $middleware->handle($request, function ($req) {
+        return response('next');
+    });
+
+    expect($response->getStatusCode())->toBe(302)
+        ->and($response->getTargetUrl())->toContain('/app');
+});
+
+it('does not redirect non-root paths', function () {
+    config(['tenancy.central_domains' => ['amsolutions.lat']]);
+
+    $middleware = new RootRedirect();
+    $request = Request::create('http://amsolutions.lat/some-other-path', 'GET');
+
+    $response = $middleware->handle($request, function ($req) {
+        return response('next');
+    });
+
+    expect($response->getStatusCode())->toBe(200);
+});
