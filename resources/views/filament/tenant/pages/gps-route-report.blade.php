045a93f613de36cc6229205691110dd943268519
ArmandoCH85 <armando.correah@gmail.com>
2026-04-13 14:58:34 -0500

    fix mapa reporte

--- resources/views/filament/tenant/pages/gps-route-report.blade.php
+++ resources/views/filament/tenant/pages/gps-route-report.blade.php
@@ -1,5 +1,6 @@
 <x-filament-panels::page>
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" />
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
 
     <style>
         .gps-report-card {
