6b5a0be15d7115de4e5e2bf8bbab6856152b6165
ArmandoCH85 <armando.correah@gmail.com>
2026-04-14 02:26:12 -0500

    fic reporte

--- app/Exports/GpsTrackExport.php
+++ app/Exports/GpsTrackExport.php
@@ -6,13 +6,10 @@ namespace App\Exports;
 
 use App\Services\GpsRouteReportService;
 use Illuminate\Support\Collection;
-use Maatwebsite\Excel\Concerns\Exportable;
 use Maatwebsite\Excel\Concerns\WithMultipleSheets;
 
 class GpsTrackExport implements WithMultipleSheets
 {
-    use Exportable;
-
     public function __construct(
         private readonly array $deviceInfo,
         private readonly Collection $points,
